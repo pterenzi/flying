4ca14b83179e9c07bf3aa6c628e8a40eaab14122
marcioa1 <marcioa1@gmail.com>
2011-10-12 19:57:03 -0300

    voos no show do cliente

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,13 +1,14 @@
 class Client < ActiveRecord::Base
-  has_many :fligths
+
+  # has_many :fligths
   has_many :balances
   
   def sales
     Account.where(:client_id => self.id)
   end
   
-  def credit
-    
+  def flights
+    Flight.where(:client_id => self.id)
   end
   
 end
