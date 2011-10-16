aa29177824d386a5c0e6c3c89031c2e8497d8f80
marcioa1 <marcioa1@gmail.com>
2011-10-16 10:49:17 -0200

    método master? em user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,9 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation#, :remember_me
+  
+  def master?
+    self.level == 1
+  end
+  
 end
